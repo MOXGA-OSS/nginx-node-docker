c1f95217e5366857b62c52649226f8c3212da96e
maxnus <cpfapp@gmail.com>
2018-01-27 00:55:16 +0700

    Fixed bugs on custom ports

--- start.sh
+++ start.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Check NODE_PORT variable is exist
+
+if [[ -z "${NODE_PORT}" ]]; then
+  NODE_PORT="3000"
+else
+  NODE_PORT="${NODE_PORT}"
+fi
+
+# Map NODE_PORT to Nginx default.conf
+
+sed -i "s/___NODE_PORT___/$NODE_PORT/g" /etc/nginx/conf.d/default.conf
+
 # Update nginx to match worker_processes to no. of cpu's
 procs=$(cat /proc/cpuinfo |grep processor | wc -l)
 sed -i -e "s/worker_processes  1/worker_processes $procs/" /etc/nginx/nginx.conf
