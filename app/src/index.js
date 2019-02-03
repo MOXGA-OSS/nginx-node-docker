9654920e790787345266cc747d5e56547fd46662
maxnus <cpfapp@gmail.com>
2019-02-03 12:01:35 +0700

    Updated license years

--- app/src/index.js
+++ app/src/index.js
@@ -1,4 +1,4 @@
-/* Copyright 2018 Finiz Open Source Software
+/* Copyright 2019 Finiz Open Source Software
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
